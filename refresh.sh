52d8f7f6c6c5cd7a83f9eee76802d184c44f496e
ptousig <ptousig@outlook.com>
2026-03-08 11:01:59 -0700

    Adding --comma-delimiter to du as well

--- refresh.sh
+++ refresh.sh
@@ -5,3 +5,4 @@ cp ../coreutils/src/coreutils-9.10/src/ls.c src/ls.c
 cp ../coreutils/src/coreutils-9.10/lib/human.h lib/human.h
 cp ../coreutils/src/coreutils-9.10/lib/human.c lib/human.c
 cp ../coreutils/src/coreutils-9.10/src/df.c src/df.c
+cp ../coreutils/src/coreutils-9.10/src/du.c src/du.c
